6fe2ab50c240182a41c824ffa8e974f86e022f08
st117171 <st117171@student.spbu.ru>
2024-09-24 05:31:19 +0300

    Enhance input validation in correct() to handle EOF (Ctrl+D) gracefully

--- 1_homework_text_editor/helper.cpp
+++ 1_homework_text_editor/helper.cpp
@@ -4,9 +4,16 @@
 #include <limits>
 
 void correct(int &checkNumber) {
-  while (!(std::cin >> checkNumber)) {
+  while (true) {
     std::cout << "Please enter a valid number: ";
-    std::cin.clear();
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    if (std::cin >> checkNumber) {
+      break;
+    } else if (std::cin.eof()) {
+      std::cout << "End of input detected. Exiting...\n";
+      exit(EXIT_FAILURE);
+    } else {
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
   }
 }
