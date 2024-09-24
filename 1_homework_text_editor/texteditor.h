7edcac49265ce4c79de9078329a7412c75f66b69
st117171 <st117171@student.spbu.ru>
2024-09-24 04:35:06 +0300

    Delete comments

--- 1_homework_text_editor/texteditor.h
+++ 1_homework_text_editor/texteditor.h
@@ -19,11 +19,9 @@ class TextEditor {
 
     friend std::ostream &operator<<(std::ostream &stream, const TextEditor &texteditor)
     {
-        // Копируем стеки, чтобы не менять их порядок
         std::stack<std::string> leftCopy = texteditor.left;
         std::stack<std::string> rightCopy = texteditor.right;
 
-        // Выводим символы слева от курсора в правильном порядке
         std::stack<std::string> reverseLeft;
         while (!leftCopy.empty())
         {
@@ -37,10 +35,8 @@ class TextEditor {
             reverseLeft.pop();
         }
 
-        // Выводим курсор
         stream << "\033[31m|\033[0m";
 
-        // Выводим символы справа от курсора
         while (!rightCopy.empty())
         {
             stream << rightCopy.top();
@@ -51,21 +47,17 @@ class TextEditor {
     }
     friend TextEditor operator+(const TextEditor &lhs, const TextEditor &rhs)
     {
-        // Создаем новый объект TextEditor, который будет результатом сложения
         TextEditor result(lhs);
 
-        // Обрабатываем текст справа от курсора правого операнда (rhs)
         std::stack<std::string> rightCopy = rhs.right; // Копируем правый стек для сохранения оригинала
         std::stack<std::string> reversedRight;
 
-        // Перевернем правый стек для правильного добавления текста в порядке
         while (!rightCopy.empty())
         {
             reversedRight.push(rightCopy.top());
             rightCopy.pop();
         }
 
-        // Добавляем перевёрнутый текст в правую часть результирующего редактора
         while (!reversedRight.empty())
         {
             result.addText(reversedRight.top());
